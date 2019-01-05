5640a0c7958ca60fec11ddcf8d2313bb9935d3b6
dayoaj <dayozhaven@gmail.com>
2019-01-05 10:53:35 +0100

    Add routes

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-import http from "http";
-import express from "express";
-import bodyParser from "body-parser";
-import morgan from "morgan";
+import http from 'http';
+import express from 'express';
+import morgan from 'morgan';
+import routes from './app/controller';
+// import meetups from './app/controller/MeetupController';
 
 const app = express();
 app.server = http.createServer(app);
 
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(morgan('dev'));
 
-app.get("/", (req, res) =>
-  res
-    .status(200)
-    .send({ message: "YAY! Congratulations! Your first endpoint is working" })
-);
+// app.get('/', (req, res) => res.status(200).send({ message: 'Server is up!' }));
+app.use('/api/v1', routes());
 
 app.server.listen(process.env.PORT || 8080, () => {
   console.log(`Started on port ${app.server.address().port}`);
 });
-
-module.exports = app;
