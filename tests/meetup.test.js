0593be100ae559070601b20930595695d0fb0479
dayoaj <dayozhaven@gmail.com>
2019-01-07 06:11:03 +0100

    Add meetup endpoint tests

--- tests/meetup.test.js
+++ tests/meetup.test.js
@@ -1,66 +1,61 @@
-import { chai } from "chai";
-import { chaiHttp } from "chai-http";
-import server from "../server";
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-const expect = chai.expect();
-
+const server = 'localhost:8080/api/v1';
+const { expect } = chai;
 chai.use(chaiHttp);
 
-describe("Meetup", () => {
+describe('Meetup', () => {
   let req = {
     body: {}
   };
 
-  describe("GET /meetup/", () => {
-    it("it should GET all the meetups", done => {
+  describe('GET /meetups', () => {
+    it('it should GET all meetups', done => {
       chai
         .request(server)
-        .get("/api/v1/meetup")
+        .get('/meetups')
         .end((err, res) => {
           expect(res).to.have.status(200);
-          expect(res.body).to.be.an("object");
-          expect(res.body).to.have.property("status");
-          expect(res.body).to.have.property("data");
-          expect(res.body.status).to.be.a("number");
-          expect(res.body.data).to.be.an("array");
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.property('status');
+          expect(res.body).to.have.property('data');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.data).to.be.an('array');
           done();
         });
     });
   });
 
-  describe("POST /meetup", () => {
-    it("it should create a meetup record", done => {
+  describe('POST /meetups', () => {
+    it('it should create a meetup record', done => {
       req = {
         body: {
-          location: "CCHub, Yaba, Lagos.",
-          topic: "Python Web Developers Meetup",
-          happeningOn: "2018-12-23T12:00:00.511Z",
-          tags: ["Programming", "Python", "Web"],
-          convener: "James Richard"
+          location: 'CCHub, Yaba, Lagos.',
+          topic: 'Python Web Developers Meetup',
+          happeningOn: '2018-12-23T12:00:00.511Z',
+          tags: ['Programming', 'Python', 'Web'],
+          convener: 'James Richard',
+          body: 'True Pythonistas Meetup!'
         }
       };
       chai
         .request(server)
-        .post("/api/v1/meetup")
+        .post('/meetups')
         .send(req.body)
         .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.body).to.be.an("object");
-          expect(res.body).to.have.property("status");
-          expect(res.body).to.have.property("data");
-          expect(res.body.status).to.be.a("number");
-          expect(res.body.data).to.be.an("array");
-          expect(res.body.data.topic).to.be.a("string");
-          expect(res.body.data.happeningOn).to.be.a("date");
-          expect(res.body.data.tags).to.be.an("array");
-          expect(res.body.data.convener).to.be.an("string");
-          expect(res.body.status).to.equal(200);
-          expect(res.body.data.topic).to.equal("Python Web Developers Meetup");
-          expect(res.body.data.happeningOn).to.equal(
-            "2018-12-23T12:00:00.511Z"
-          );
-          expect(res.body.data.tags).to.equal(["Programming", "Python", "Web"]);
-          expect(res.body.data.convener).to.equal("James Richard");
+          expect(res).to.have.status(201);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.property('status');
+          expect(res.body).to.have.property('data');
+          expect(res.body.status).to.be.a('number');
+          expect(res.body.data).to.be.an('array');
+          expect(res.body.data[0].topic).to.be.a('string');
+          expect(res.body.data[0].happeningOn).to.be.a('string');
+          expect(res.body.data[0].tags).to.be.an('array');
+          expect(res.body.status).to.equal(201);
+          expect(res.body.data[0].topic).to.equal('Python Web Developers Meetup');
+          expect(res.body.data[0].happeningOn).to.equal('2018-12-23T12:00:00.511Z');
           done();
         });
     });
