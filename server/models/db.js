b1b9db880e204ef1aa97f85e34f56742570d8423
dayoaj <dayozhaven@gmail.com>
2019-01-15 17:25:47 +0100

    [Chore #163115064] Setup Database

--- server/models/db.js
+++ server/models/db.js
@@ -1,38 +1,146 @@
-const questions = [];
-const meetups = [];
-const rsvps = [];
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
-class obj {
-  static setQuestions(data) {
-    return questions.push(data);
-  }
+dotenv.config();
 
-  static setMeetups(data) {
-    return meetups.push(data);
-  }
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 5,
+  idleTimeoutMillis: 1000,
+  connectionTimeoutMillis: 2000
+});
 
-  static setRsvps(data) {
-    return rsvps.push(data);
-  }
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
 
-  static getQuestions() {
-    return questions;
-  }
+/**
+ * Create All Tables
+ */
+const queryCreateUser = `CREATE TABLE IF NOT EXISTS
+  users(
+    id UUID,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    othername VARCHAR(50),
+    email VARCHAR(50),
+    phoneNumber VARCHAR(30),
+    username VARCHAR(30),
+    registered timestamp DEFAULT CURRENT_TIMESTAMP,
+    isAdmin boolean,
+    PRIMARY KEY (id),
+    UNIQUE (username)
+  )`;
 
-  static getMeetups() {
-    return meetups;
-  }
+const queryCreateMeetup = `CREATE TABLE IF NOT EXISTS
+    meetups(
+    id UUID,
+    user_id UUID NOT NULL,
+    createdOn timestamp DEFAULT CURRENT_TIMESTAMP,
+    location VARCHAR(60),
+    images text[],
+    topic VARCHAR (50),
+    body VARCHAR (255),
+    happeningOn timestamp NOT NULL,
+    tags text[],
+    convener VARCHAR(50),
+    PRIMARY KEY (id),
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+    )`;
 
-  static getRsvps() {
-    return rsvps;
-  }
+const queryCreateQuestion = `CREATE TABLE IF NOT EXISTS
+    questions(
+    id UUID,
+    createdOn timestamp DEFAULT CURRENT_TIMESTAMP,
+    createdBy UUID NOT NULL,
+    meetup UUID NOT NULL,
+    title VARCHAR (50),
+    body VARCHAR (255),
+    votes int DEFAULT 0,
+    PRIMARY KEY (id),
+    FOREIGN KEY(createdBy) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(meetup) REFERENCES meetups(id) ON DELETE CASCADE
+    )`;
 
-  static refresh() {
-    questions.splice(0, questions.length);
-    meetups.splice(0, meetups.length);
-    rsvps.splice(0, rsvps.length);
-    return null;
-  }
-}
+const queryCreateRSVP = `CREATE TABLE IF NOT EXISTS
+    rsvps( 
+    id UUID,
+    meetup  UUID NOT NULL,
+    user_id UUID NOT NULL,
+    response VARCHAR(10),
+    PRIMARY KEY (id),
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(meetup) REFERENCES meetups(id) ON DELETE CASCADE
+    )`;
 
-export default obj;
+const queryCreateComment = `CREATE TABLE IF NOT EXISTS
+    comments(
+    id UUID,
+    question UUID NOT NULL,
+    createdBy UUID NOT NULL,
+    createdOn timestamp DEFAULT CURRENT_TIMESTAMP,
+    body VARCHAR (255),
+    PRIMARY KEY (id),
+    FOREIGN KEY(createdBy) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(question) REFERENCES questions(id) ON DELETE CASCADE
+    )`;
+
+pool
+  .query(queryCreateUser)
+  .then(res => {
+    console.log(res);
+    pool.end();
+  })
+  .catch(err => {
+    console.log(err);
+    pool.end();
+  });
+
+pool
+  .query(queryCreateMeetup)
+  .then(res => {
+    console.log(res);
+    pool.end();
+  })
+  .catch(err => {
+    console.log(err);
+    pool.end();
+  });
+
+pool
+  .query(queryCreateQuestion)
+  .then(res => {
+    console.log(res);
+    pool.end();
+  })
+  .catch(err => {
+    console.log(err);
+    pool.end();
+  });
+
+pool
+  .query(queryCreateRSVP)
+  .then(res => {
+    console.log(res);
+    pool.end();
+  })
+  .catch(err => {
+    console.log(err);
+    pool.end();
+  });
+
+pool
+  .query(queryCreateComment)
+  .then(res => {
+    console.log(res);
+    pool.end();
+  })
+  .catch(err => {
+    console.log(err);
+    pool.end();
+  });
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
